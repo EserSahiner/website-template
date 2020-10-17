0d594c1f664d86e08a9b5a88d9fefae218fc863d
Zesha <esershnr@gmail.com>
2020-10-18 02:53:27 +0300

    Responsive: Animation Trial

--- js/responsive.js
+++ js/responsive.js
@@ -1,6 +1,21 @@
-$(document).ready(function(){
-    $('.menu-toggle').click(function(){
-        $('.menu-toggle').toggleClass('responsive')
-        $('#navbar-ul').toggleClass('responsive')
-    });
+
+//jQuery Toggling
+
+//$(document).ready(function(){
+//    $('.menu-toggle').on("click",function(){
+//        $('.menu-toggle').toggleClass('responsive', 1000);
+//        $('#navbar-ul').toggleClass('responsive', 1000);
+//    });
+//});
+
+
+const btnRes = document.querySelector('.menu-toggle');
+
+const ulMenu = document.querySelector('#navbar-ul');
+
+
+btnRes.addEventListener('click', () => {
+    ulMenu.classList.toggle('responsive');
+    ulMenu.classList.toggle('fade');
+    btnRes.classList.toggle('responsive');
 });

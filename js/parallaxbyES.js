a4a1ec707d03129699a5b4b75100902c25d37145
Zesha <esershnr@gmail.com>
2020-10-26 12:16:40 +0300

    Grid Disabled & Parallax Work Added

---
+++ js/parallaxbyES.js
@@ -0,0 +1,34 @@
+const parallaxBox = document.querySelector('.parallax-container');
+
+parallaxBox.addEventListener('pointermove' , parallax)
+
+function parallax(e) {
+    this.querySelectorAll('.pitem').forEach(q => {
+        const speed = q.getAttribute('data-speed')
+
+        const x = (parallaxBox.style.width - e.pageX*speed) / 50;
+        const y = (parallaxBox.style.height - e.pageY*speed) / 50;
+
+        
+
+        q.style.transform = `translateX(${x}px) translateY(${y}px)`
+        console.log(e);
+    });
+};
+
+parallaxBox.addEventListener('pointerleave', panimationOut);
+
+function panimationOut(){
+    this.querySelectorAll('.pitem').forEach(q => {
+        q.style.transition = 'all 1s ease-out';
+        q.style.transform = 'none';
+    })
+}
+
+parallaxBox.addEventListener('pointerenter', panimationIn);
+
+function panimationIn(){
+    this.querySelectorAll('.pitem').forEach(q => {
+        q.style.transition= 'none';
+    })
+}
